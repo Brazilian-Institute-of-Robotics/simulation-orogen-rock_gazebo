5fcc07eebc53dd440422f161badd8517d2466670
gustavoneves12 <gustavo.neves12@gmail.com>
2015-08-10 15:25:14 -0300

    change the readInput function to translate data from gazebo to rock structure base::samples::LaserScan properly

--- tasks/LaserScanTask.cpp
+++ tasks/LaserScanTask.cpp
@@ -6,6 +6,8 @@ using namespace std;
 using namespace gazebo;
 using namespace rock_gazebo;
 
+#define meters_to_milimeters(x) (x) * 1000
+
 LaserScanTask::LaserScanTask(std::string const& name)
     : LaserScanTaskBase(name)
 {
@@ -84,20 +86,39 @@ void LaserScanTask::readInput( ConstLaserScanStampedPtr & laserScanMSG )
 {
     laserScanCMD.ranges.clear();
 
-    for(int i = 0; i < laserScanMSG->scan().ranges_size(); ++i)
-        laserScanCMD.ranges.push_back( laserScanMSG->scan().ranges(i) * 1000);
+    if( !laserScanMSG->scan().has_range_max() )
+        throw std::runtime_error("rock_gazebo::LaserScanTask requires range_max to be set");
 
-    if( laserScanMSG->scan().has_angle_step() )
-        laserScanCMD.angular_resolution = laserScanMSG->scan().angle_step();
+    if( !laserScanMSG->scan().has_range_min() )
+        throw std::runtime_error("rock_gazebo::LaserScanTask requires range_min to be set");
 
-    if( laserScanMSG->scan().has_range_min() )
-        laserScanCMD.minRange = laserScanMSG->scan().range_min() * 1000;
+    if( !laserScanMSG->scan().has_angle_step() )
+        throw std::runtime_error("rock_gazebo::LaserScanTask requires angle_step to be set");
 
-    if( laserScanMSG->scan().has_range_max() )
-        laserScanCMD.maxRange = laserScanMSG->scan().range_max() * 1000;
+    if( !laserScanMSG->scan().has_angle_min() )
+        throw std::runtime_error("rock_gazebo::LaserScanTask requires angle_min to be set");
 
-    if( laserScanMSG->scan().has_angle_min() )
-        laserScanCMD.start_angle = laserScanMSG->scan().angle_min();
+    double range_min = laserScanMSG->scan().range_min();
+    double range_max = laserScanMSG->scan().range_max();
+
+    laserScanCMD.minRange = meters_to_milimeters(range_min);
+    laserScanCMD.maxRange = meters_to_milimeters(range_max);
+    laserScanCMD.angular_resolution = laserScanMSG->scan().angle_step();
+    laserScanCMD.start_angle = laserScanMSG->scan().angle_min();
+
+    for(int i = 0; i < laserScanMSG->scan().ranges_size(); ++i) {
+        double range = laserScanMSG->scan().ranges(i);
+
+        if (range >= range_max) {
+            laserScanCMD.ranges.push_back(base::samples::TOO_FAR);
+        }
+        else if (range <= range_min) {
+            laserScanCMD.ranges.push_back(base::samples::TOO_NEAR);
+        }
+        else {
+            laserScanCMD.ranges.push_back(meters_to_milimeters(range));
+        }
+    }
 }
 
 
